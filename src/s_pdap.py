26a60ff42e19dd7f93e0c79d3b0c34a9012fad86
arsen-hnatiuk <arsen.hnatiuk@hu-berlin.de>
2024-09-06 17:17:56 +0200

    fix: bugs

--- src/s_pdap.py
+++ src/s_pdap.py
@@ -19,8 +19,8 @@ class SPDAP_Finite:
         self,
         alpha: float,
         data_path: str,
-        test_power: float = 0.05,
-        parameter_increase: float = 0.1,
+        # test_power: float = 0.05,
+        # parameter_increase: float = 0.1,
     ) -> None:
         target = pd.read_csv(data_path)["log kappa_L"].to_numpy()
         self.target_norm = np.linalg.norm(target)
@@ -28,35 +28,36 @@ class SPDAP_Finite:
         self.alpha = alpha
         # self.test_power = test_power
         # self.parameter_increase = parameter_increase
+        self.ops = [
+            "add",
+            "sub",
+            "abs_diff",
+            "mult",
+            "div",
+            "inv",
+            "abs",
+            "exp",
+            "log",
+            "sin",
+            "cos",
+            "sq",
+            "cb",
+            "six_pow",
+            "sqrt",
+            "cbrt",
+            "neg_exp",
+        ]
         self.feature_inputs = import_dataframe.create_inputs(
-            df="thermal_conductivity_data.csv",
+            df=data_path,
             max_rung=3,
             max_param_depth=0,
             prop_key="log kappa_L",
             calc_type="regression",
             n_sis_select=10,
-            allowed_ops=[],
+            allowed_ops=self.ops,
             n_rung_generate=0,
             n_rung_store=-1,
-            allowed_param_ops=[
-                "add",
-                "sub",
-                "abs_diff",
-                "mult",
-                "div",
-                "inv",
-                "abs",
-                "exp",
-                "log",
-                "sin",
-                "cos",
-                "sq",
-                "cb",
-                "six_pow",
-                "sqrt",
-                "cbrt",
-                "neg_exp",
-            ],
+            allowed_param_ops=[],
             global_param_opt=False,
             reparam_residual=False,
         )
@@ -208,55 +209,119 @@ class SPDAP_Finite:
             iterate *= probability
         return size
 
-    def find_x_tilde(
-        self, rho: np.ndarray, tol: float, probability_tolerance: float
+    def generate_sample_and_determine_incumbent(
+        self, running_dict: dict, criterion: float, rho: np.ndarray
     ) -> tuple:
-        incumbent_dict = {"value": -1}
-        generated_sample_size = 0
-        null_beta = self.apha * (1 + tol) / np.linalg.norm(rho)
+        incumbent_dict = {}
+
+        # Generate new sample
+        feature_space = FeatureSpace(self.feature_inputs)
+        sample_features_raw = np.array([feature.value for feature in feature_space.phi])
+        sample_norms = np.linalg.norm(sample_features_raw, axis=1)
+        sample_features = np.array(
+            [feature / norm for feature, norm in zip(sample_features_raw, sample_norms)]
+        )
+        sample_values = np.abs(np.matmul(sample_features, rho))
+        sample_expressions = np.array([feature.expr for feature in feature_space.phi])
+
+        # Add sample to running dict
+        if len(running_dict["expressions"]):
+            running_dict["K_T"] = np.vstack((running_dict["K_T"], sample_features))
+            running_dict["norms"] = np.append(running_dict["norms"], sample_norms)
+            running_dict["expressions"] = np.append(
+                running_dict["expressions"], sample_expressions
+            )
+            running_dict["values"] = np.append(running_dict["values"], sample_values)
+        else:
+            running_dict["K_T"] = sample_features
+            running_dict["norms"] = sample_norms
+            running_dict["expressions"] = sample_expressions
+            running_dict["values"] = sample_values
+
+        # Determine incumbent (best feature so far)
+        incumbent = np.argmax(sample_values)
+        max_value = sample_values[incumbent]
+        if max_value > criterion:
+            incumbent_dict["value"] = max_value
+            incumbent_dict["feature"] = sample_features[incumbent]
+            incumbent_dict["feature_norm"] = sample_norms[incumbent]
+            incumbent_dict["expression"] = feature_space.phi[incumbent].expr
+            incumbent_dict["Phi_value"] = self.M * (
+                incumbent_dict["value"] - self.alpha
+            )
+
+        return incumbent_dict, running_dict
+
+    def find_x_tilde(
+        self,
+        rho: np.ndarray,
+        tol: float,
+        probability_tolerance: float,
+        running_dict: dict,
+    ) -> tuple:
+        # Check best existing feature
+        if len(running_dict["expressions"]):
+            incumbent = np.argmax(running_dict["values"])
+            incumbent_dict = {
+                "value": running_dict["values"][incumbent],
+                "feature": running_dict["K_T"][incumbent],
+                "feature_norm": running_dict["norms"][incumbent],
+                "expression": running_dict["expressions"][incumbent],
+                "Phi_value": self.M * (incumbent_dict["value"] - self.alpha),
+            }
+        else:
+            incumbent_dict = {"value": -1}
+
+        # Build test parameters
+        null_beta = self.alpha * (1 + tol) / np.linalg.norm(rho)
         if null_beta >= 1:
             # The tolerance has been reached
-            return incumbent_dict, generated_sample_size
-        alternative_beta = self.apha / np.linalg.norm(rho)
+            return incumbent_dict, running_dict
+        alternative_beta = self.alpha / np.linalg.norm(rho)
         interval_probability = self.compute_probability(null_beta, alternative_beta)
         sample_size = self.compute_sample_size(
             interval_probability, probability_tolerance
         )
+        logging.info(f"Sample size for x_tilde: {sample_size}")
+
         while (
-            generated_sample_size < sample_size
+            len(running_dict["expressions"]) < sample_size
             and incumbent_dict["value"] <= alternative_beta
         ):
-            feature_space = FeatureSpace(self.feature_inputs)
-            sample_features_raw = np.array(
-                [feature.value for feature in feature_space.phi]
+            incumbent_dict, running_dict = self.generate_sample_and_determine_incumbent(
+                running_dict, alternative_beta, rho
             )
-            sample_norms = np.linalg.norm(sample_features_raw, axis=1)
-            sample_features = np.array(
-                [
-                    feature / norm
-                    for feature, norm in zip(sample_features_raw, sample_norms)
-                ]
+            logging.info(
+                f"Generated sample so far: {len(running_dict['expressions']) }"
             )
-            generated_sample_size += len(sample_features)
-            sample_values = np.abs(np.matmul(sample_features, rho))
-            incumbent = np.argmax(sample_values)
-            max_value = sample_values[incumbent]
-            if max_value > alternative_beta:
-                incumbent_dict["value"] = max_value
-                incumbent_dict["feature"] = sample_features[incumbent]
-                incumbent_dict["feature_norm"] = sample_norms[incumbent]
-                incumbent_dict["expression"] = feature_space.phi[incumbent].expr
-                incumbent_dict["Phi_value"] = self.M * (
-                    incumbent_dict["value"] - self.alpha
-                )
-        return incumbent_dict, generated_sample_size
+        return incumbent_dict, running_dict
 
     def find_x_k(
-        self, x_tilde_dict: dict, rho: np.ndarray, generated_sample_size: int
-    ) -> dict:
-        target_sample_size = generated_sample_size + 1
+        self, x_tilde_dict: dict, rho: np.ndarray, running_dict: dict
+    ) -> tuple:
+        # Generate needed sample size
         incumbent_dict = x_tilde_dict
-        while generated_sample_size < target_sample_size:
+        P_incumbent = incumbent_dict["value"]  # P_k(x_k)
+        interval_bound = 1 - incumbent_dict["Phi_value"] / (
+            2 * self.M * np.linalg.norm(rho)
+        )
+        interval_probability = self.compute_probability(1, interval_bound)
+        probability_tolerance = 1 - (
+            (P_incumbent + 3 * self.alpha) / (2 * P_incumbent + 2 * self.alpha)
+        )
+        target_sample_size = self.compute_sample_size(
+            interval_probability, probability_tolerance
+        )
+        logging.info(f"Target sample size for x_k: {target_sample_size}")
+        while len(running_dict["expressions"]) < target_sample_size:
+            incumbent_dict, running_dict = self.generate_sample_and_determine_incumbent(
+                running_dict, incumbent_dict["value"], rho
+            )
+            logging.info(
+                f"Generated sample so far: {len(running_dict['expressions']) }"
+            )
+
+            # Compute new needed sample size
             P_incumbent = incumbent_dict["value"]  # P_k(x_k)
             interval_bound = 1 - incumbent_dict["Phi_value"] / (
                 2 * self.M * np.linalg.norm(rho)
@@ -268,64 +333,58 @@ class SPDAP_Finite:
             target_sample_size = self.compute_sample_size(
                 interval_probability, probability_tolerance
             )
-            feature_space = FeatureSpace(self.feature_inputs)
-            sample_features_raw = np.array(
-                [feature.value for feature in feature_space.phi]
-            )
-            sample_norms = np.linalg.norm(sample_features_raw, axis=1)
-            sample_features = np.array(
-                [
-                    feature / norm
-                    for feature, norm in zip(sample_features_raw, sample_norms)
-                ]
-            )
-            generated_sample_size += len(sample_features)
-            sample_values = np.abs(np.matmul(sample_features, rho))
-            incumbent = np.argmax(sample_values)
-            max_value = sample_values[incumbent]
-            if max_value > incumbent_dict["value"]:
-                incumbent_dict["value"] = max_value
-                incumbent_dict["feature"] = sample_features[incumbent]
-                incumbent_dict["feature_norm"] = sample_norms[incumbent]
-                incumbent_dict["expression"] = feature_space.phi[incumbent].expr
-                incumbent_dict["Phi_value"] = self.M * (
-                    incumbent_dict["value"] - self.alpha
-                )
-        return incumbent_dict
+            logging.info(f"Target sample size for x_k: {target_sample_size}")
+        return incumbent_dict, running_dict
 
     def solve_exact(self, tol: float, confidence: float) -> dict:
         u = np.array([])
-        active_K_T = np.array([])
-        active_norms = np.array([])
-        active_expressions = np.array([])
+        active_dict = {
+            "K_T": np.array([]),
+            "norms": np.array([]),
+            "expressions": np.array([]),
+        }
+        running_dict = {
+            "K_T": np.array([]),
+            "norms": np.array([]),
+            "expressions": np.array([]),
+            "values": np.array([]),
+        }
         rho = self.rho(np.zeros((len(self.target), 1)), np.zeros(1))
         k = 0
         while True:
             # Test optimality
-            x_tilde_dict, generated_sample_size = self.find_x_tilde(
-                rho, tol, 1 - confidence
+            x_tilde_dict, running_dict = self.find_x_tilde(
+                rho, tol, 1 - confidence, running_dict
             )
             if x_tilde_dict["value"] < self.alpha / np.linalg.norm(rho):
                 # optimality reached
                 break
 
             # Update the iterate
-            x_dict = self.find_x_k(x_tilde_dict, rho, generated_sample_size)
+            x_dict, running_dict = self.find_x_k(x_tilde_dict, rho, running_dict)
             eta_k = 8 / (k + 7)
-            if x_dict["expression"] in active_expressions:
-                index = np.where(active_expressions == x_dict["expressions"])[0][0]
+            if x_dict["expression"] in active_dict["expressions"]:
+                index = np.where(active_dict["expressions"] == x_dict["expressions"])[
+                    0
+                ][0]
                 v = (
                     self.M
                     * np.sign(np.dot(rho, x_dict["feature"]))
                     * np.eye(1, len(u), index)[0]
                 )
             else:
-                if len(active_K_T):
-                    active_K_T = np.vstack((active_K_T, x_dict["feature"]))
+                if len(active_dict["K_T"]):
+                    active_dict["K_T"] = np.vstack(
+                        (active_dict["K_T"], x_dict["feature"])
+                    )
                 else:
-                    active_K_T = np.array([x_dict["feature"]])
-                active_norms = np.append(active_norms, x_dict["feature_norm"])
-                active_expressions = np.append(active_expressions, x_dict["expression"])
+                    active_dict["K_T"] = np.array([x_dict["feature"]])
+                active_dict["norms"] = np.append(
+                    active_dict["norms"], x_dict["feature_norm"]
+                )
+                active_dict["expressions"] = np.append(
+                    active_dict["expressions"], x_dict["expression"]
+                )
                 u = np.append(u, np.array([0]))
                 v = (
                     self.M
@@ -335,23 +394,33 @@ class SPDAP_Finite:
             u = (1 - eta_k) * u + eta_k * v
 
             # Low-dimensional optimization
-            ssn = SSN(K=active_K_T.T, alpha=self.alpha, target=self.target, M=self.M)
+            ssn = SSN(
+                K=active_dict["K_T"].T, alpha=self.alpha, target=self.target, M=self.M
+            )
             u_raw = ssn.solve(tol=self.machine_precision, u_0=u)
             u_to_keep = np.where(np.abs(u_raw) >= self.machine_precision)[0]
             u = u_raw[u_to_keep]
-            active_K_T = active_K_T[u_to_keep]
-            active_norms = active_norms[u_to_keep]
-            active_expressions = active_expressions[u_to_keep]
-            rho = self.rho(active_K_T.T, u)
+            active_dict["K_T"] = active_dict["K_T"][u_to_keep]
+            active_dict["norms"] = active_dict["norms"][u_to_keep]
+            active_dict["expressions"] = active_dict["expressions"][u_to_keep]
+            rho = self.rho(active_dict["K_T"].T, u)
+            running_dict["values"] = np.abs(np.matmul(running_dict["K_T"], rho))
 
             k += 1
-            logging.info(f"{k}: Phi_gap {x_dict["Phi_value"]/self.M:.3E}, alternative_beta: {self.apha / np.linalg.norm(rho)}, support {len(u)}")
+            logging.info(
+                f"{k}: Phi_gap {x_dict['Phi_value']/self.M:.3E}, alternative_beta: {self.alpha / np.linalg.norm(rho)}, support {len(u)}"
+            )
         logging.info(
             f"LGCG converged in {k} iterations to tolerance {tol:.3E} with confidence {confidence} and final sparsity of {len(u)}"
         )
         # Rescale the solution
-        for ind, pos in enumerate(active_norms):
+        for ind, pos in enumerate(active_dict["norms"]):
             u[ind] /= pos
         u = u * self.target_norm
 
-        return {"u": u, "support": len(u), "expressions": active_expressions}
+        return {"u": u, "support": len(u), "expressions": active_dict["expressions"]}
+
+
+if __name__ == "__main__":
+    exp = SPDAP_Finite(0.1, "data/thermal_conductivity_data.csv")
+    exp.solve_exact(tol=0.001, confidence=0.999)
